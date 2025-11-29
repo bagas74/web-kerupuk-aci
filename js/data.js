34db680f4862776a8d50dbc04ead331cba1fdc24
bagas74 <bagassatrio744@gmail.com>
2025-11-29 22:46:53 +0700

    first commit

---
+++ js/data.js
@@ -0,0 +1,68 @@
+// js/data.js
+
+function shopData() {
+  return {
+    // --- DATA KONTAK ---
+    contact: {
+      wa: "6281234567890",
+      ig: "https://instagram.com/krupukaci_enak",
+      tiktok: "https://tiktok.com/@krupukaci_viral",
+      email: "halo@krupukaci.com",
+      address: "Jl. Krupuk Renyah No. 88, Kota Bandung.",
+      // Link embed peta (disingkat biar rapi)
+      map_embed:
+        "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7910.057793946626!2d110.796836287773!3d-7.57182838444058!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e7a16783dbd32eb%3A0xe852ba0aa1842158!2sUniversitas%20Duta%20Bangsa%20(Kampus%201%20Bhayangkara)!5e0!3m2!1sid!2sid!4v1764332059242!5m2!1sid!2sid",
+    },
+
+    // --- DATA PRODUK ---
+    products: [
+      {
+        name: "Krupuk Aci Original",
+        desc: "Renyah gurih dengan bumbu bawang asli.",
+        detail:
+          "Resep warisan sejak 1990. Rasa bawangnya kuat namun tidak bikin eneg.",
+        price: "Rp 15.000",
+        weight: "250 Gram",
+        composition: "Tepung Tapioka, Bawang Putih, Garam",
+        image: "img/krupuk-ori.jpg",
+      },
+      {
+        name: "Krupuk Pedas Jeletot",
+        desc: "Varian pedas nampol dengan cabai kering.",
+        detail:
+          "Khusus pecinta pedas! Cabai rawit merah asli yang dikeringkan.",
+        price: "Rp 18.000",
+        weight: "200 Gram",
+        composition: "Tepung Tapioka, Cabai, Bawang",
+        image: "img/krupuk-pedas.jpg",
+      },
+      {
+        name: "Seblak Krupuk Kering",
+        desc: "Inovasi baru krupuk bantat bumbu kencur.",
+        detail: "Krupuk digoreng bantat diaduk bumbu kencur & daun jeruk.",
+        price: "Rp 20.000",
+        weight: "150 Gram",
+        composition: "Krupuk Aci, Kencur, Daun Jeruk",
+        image: "img/seblak.jpg",
+      },
+      {
+        name: "Seblak Krupuk b",
+        desc: "Inovasi baru krupuk bantat bumbu kencur.",
+        detail: "Krupuk digoreng bantat diaduk bumbu kencur & daun jeruk.",
+        price: "Rp 20.000",
+        weight: "150 Gram",
+        composition: "Krupuk Aci, Kencur, Daun Jeruk",
+        image: "img/seblak.jpg",
+      },
+      {
+        name: "Seblak Krupuk a",
+        desc: "Inovasi baru krupuk bantat bumbu kencur.",
+        detail: "Krupuk digoreng bantat diaduk bumbu kencur & daun jeruk.",
+        price: "Rp 20.000",
+        weight: "150 Gram",
+        composition: "Krupuk Aci, Kencur, Daun Jeruk",
+        image: "img/seblak.jpg",
+      },
+    ],
+  };
+}
